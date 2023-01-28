eb11c8dc3aba50ef1a357bf7fa3408a3ac700666
bobby.kemp <bobby.kemp@iowa80group.com>
2023-01-28 14:09:36 -0600

    tweaks for gh pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script>
-	export const prerender = true;
 	import Header from './Header.svelte';
 	import './styles.css';
 </script>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,9 @@
-const dev = process.argv.includes('dev');
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		paths: {
-			base: dev ? "" : "/about",
-		}
+		adapter: adapter()
 	}
 };
 
